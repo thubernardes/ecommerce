0591b3f65a1f6679693675fc03647db440d7012e
thubernardes <gallo.arthur@yahoo.com.br>
2022-02-08 09:33:37 -0300

    Removendo declarações não utilizadas

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
 import logo from '../../assets/logo.png';
 import useStyles from './styles';
-
 
 const Navbar = () => {
     const classes = useStyles();
